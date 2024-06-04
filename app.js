8db2abe0beb6b5bb9220f047140c6f460a152757
Chí Thành <chithanh171004@gmail.com>
2024-06-04 20:57:06 +0700

    Add cors

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 import express from "express";
+import cors from "cors";
 import connectDB from "./database.js";
 import userRouter from "./src/routers/userRouter.js";
 import homeRouter from "./src/routers/homeRouter.js";
 import taskRouter from "./src/routers/taskRouter.js";
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 connectDB();
 
